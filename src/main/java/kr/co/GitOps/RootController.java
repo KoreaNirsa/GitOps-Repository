8c062b7f1f6b217c98154e446851b5c80d1e564d
Nirsa <60082298+KoreaNirsa@users.noreply.github.com>
2026-03-05 15:01:10 +0900

    Add files via upload

---
+++ src/main/java/kr/co/GitOps/RootController.java
@@ -0,0 +1,14 @@
+package kr.co.GitOps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RrootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "버전 1.0 - cicd 테스트 입니다.";
+    }
+
+}
